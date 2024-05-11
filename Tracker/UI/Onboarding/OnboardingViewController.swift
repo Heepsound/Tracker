aaa5231cb65dec163f86f46c2103a93b8c9e9b41
Heepsound <heepsound@yandex.ru>
2024-05-11 13:16:41 +0300

    Реализован Onboarding, состояние его прохождения регистрируется в UserDefaults

---
+++ Tracker/UI/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,87 @@
+//
+//  OnboardingViewController.swift
+//  Tracker
+//
+//  Created by Владимир Горбачев on 11.05.2024.
+//
+
+import UIKit
+
+final class OnboardingViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    lazy var pages: [UIViewController] = {
+        let page1 = OnboardingPageViewController(backgroundImage: .onboardingPage1Background, title: "Отслеживайте только то, что хотите")
+        let page2 = OnboardingPageViewController(backgroundImage: .onboardingPage2Background, title: "Даже если это не литры воды и йога")
+        return [page1, page2]
+    }()
+
+    lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = 0
+        pageControl.currentPageIndicatorTintColor = .trackerBlack
+        pageControl.pageIndicatorTintColor = .trackerBlackAlpha30
+        return pageControl
+    }()
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewController()
+    }
+    
+    func setupViewController() {
+        dataSource = self
+        delegate = self
+        if let firstPage = pages.first {
+            setViewControllers([firstPage], direction: .forward, animated: true, completion: nil)
+        }
+        addSubViews()
+        applyConstraints()
+    }
+    
+    private func addSubViews() {
+        view.addSubviewWithoutAutoresizingMask(pageControl)
+    }
+
+    private func applyConstraints() {
+        NSLayoutConstraint.activate([
+            pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -140),
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    // MARK: - UIPageViewControllerDataSource
+
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController
+    ) -> UIViewController? {
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+        let previousIndex = currentIndex - 1
+        guard previousIndex >= 0 else { return pages.last }
+        return pages[previousIndex]
+    }
+
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController
+    ) -> UIViewController? {
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+        let nextIndex = currentIndex + 1
+        guard nextIndex < pages.count else { return pages.first }
+        return pages[nextIndex]
+    }
+    
+    // MARK: - UIPageViewControllerDelegate
+
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        didFinishAnimating finished: Bool,
+        previousViewControllers: [UIViewController],
+        transitionCompleted completed: Bool
+    ) {
+        if let firstPage = pageViewController.viewControllers?.first,
+           let index = pages.firstIndex(of: firstPage) {
+            pageControl.currentPage = index
+        }
+    }
+}
